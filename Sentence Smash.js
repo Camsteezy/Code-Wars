9d39476f8be2e1d0c7951fcd853b4f0db8603824
Cameron Crawley <101986843+Camsteezy@users.noreply.github.com>
2024-08-21 21:16:14 -0400

    Create Sentence Smash.js

---
+++ Sentence Smash.js
@@ -0,0 +1,11 @@
+function smash (words) {
+ 
+//   P:strings yes ? special characters ? numbers ? spaces needed ?
+//   R: array of words as one sentence with spaces
+//   E:console.log(sentence['hey','there','yall']Hey there yall)
+//   console.log(sentence['Happy','Birthday','too','you']Happy Birthday to you)
+//   P:split, join , and map
+  
+  return words.join(' ')
+  
+};
