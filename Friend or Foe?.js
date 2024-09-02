87ba8fa412863a6478a01eca848003331da6e379
Cameron Crawley <101986843+Camsteezy@users.noreply.github.com>
2024-09-01 22:22:58 -0400

    Create Friend or Foe?.js

---
+++ Friend or Foe?.js
@@ -0,0 +1,10 @@
+function friend(friends){
+  //your code here
+//   P:strings. numbers ? special character?  
+//   R: list with only names of friends
+//   E:console.log(friend['Itzel','Cam','Jeff','Chris']'Jeff')
+//   console.log(friend['Itzel','Cam']{})
+//   P: make it so 4 names gives output anything else no output. filter 
+  
+  return friends.filter((name)=> name.length === 4 )
+}
