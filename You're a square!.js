8052ca8d7b605698f7b50ab1334d7a5db2ac52a9
Cameron Crawley <101986843+Camsteezy@users.noreply.github.com>
2024-09-03 22:54:00 -0400

    Create You're a square!.js

---
+++ You're a square!.js
@@ -0,0 +1,14 @@
+var isSquare = function(n){
+  
+//   P:string. numbers ? special characters ?
+//   R: true if number is a square number or false if not
+//   E:console.log(ex[100]true)
+//   console.log(ex[7]false)
+//   P:else if staement 
+  
+  if (Math.sqrt(n) % 1 === 0){
+    return true
+  }else {
+  return false; // fix me
+    }
+}
